352829e880e8a942433ae7b008cc6d144176438b
Justin Makaila <justinmakaila@gmail.com>
2014-09-29 15:56:10 -0400

    Initial Commit

---
+++ PresentPrePermissions/PresentPrePermissions.h
@@ -0,0 +1,19 @@
+//
+//  PresentPrePermissions.h
+//  PresentPrePermissions
+//
+//  Created by Justin Makaila on 9/29/14.
+//  Copyright (c) 2014 Justin Makaila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PresentPrePermissions.
+FOUNDATION_EXPORT double PresentPrePermissionsVersionNumber;
+
+//! Project version string for PresentPrePermissions.
+FOUNDATION_EXPORT const unsigned char PresentPrePermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PresentPrePermissions/PublicHeader.h>
+
+
